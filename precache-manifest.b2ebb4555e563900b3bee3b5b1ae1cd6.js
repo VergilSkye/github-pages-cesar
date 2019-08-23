82ae6e2a3a4bab0df9e61926fa3425470b5bbaff
Virgilio <virgiliosps@gmail.com>
2019-08-23 01:41:49 -0300

    Updates

---
+++ precache-manifest.b2ebb4555e563900b3bee3b5b1ae1cd6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee23dc62b4505fc640cab0cea576a79",
+    "url": "/github-pages-cesar/index.html"
+  },
+  {
+    "revision": "7d46af7261c69adc3a49",
+    "url": "/github-pages-cesar/static/js/2.d3559978.chunk.js"
+  },
+  {
+    "revision": "71ba68f69225f11620d9",
+    "url": "/github-pages-cesar/static/js/main.7c6c8820.chunk.js"
+  },
+  {
+    "revision": "0bca0ad9461c580d6d87",
+    "url": "/github-pages-cesar/static/js/runtime~main.41912b67.js"
+  }
+]);
